9d77c96a5cabb4eac970a83729659f0197d52a64
Allan Arguello <allanoarguello@gmail.com>
2026-02-01 20:16:55 -0800

    Clean files without comments

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "AI Pilot is Online! Hello from Google Cloud Run."
+def hello():
+    return "AI Pilot is Online!"
 
 if __name__ == "__main__":
-    # Cloud Run passes the port as an environment variable
     port = int(os.environ.get("PORT", 8080))
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
